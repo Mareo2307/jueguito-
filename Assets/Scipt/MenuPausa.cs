a78ef6fb868503fabde13c54cdd467edf5b23912
Mareo2307 <mareo.deleit@gmail.com>
2023-10-06 07:53:07 -0600

    Pausa

---
+++ Assets/Scipt/MenuPausa.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuPausa : MonoBehaviour
+{
+
+    [SerializeField] private GameObject BotonPausa;
+    [SerializeField] private GameObject MenuPaus;
+    private bool JuegoPausado = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (JuegoPausado)
+            {
+                Resume();
+            }
+            else
+            {
+                Pausa();
+            }
+        }
+    }
+    public void Pausa()
+    {
+        Time.timeScale = 0f;
+        BotonPausa.SetActive(false);
+        MenuPaus.SetActive(true);
+        
+    }
+    
+    
+    public void Reiniciar()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        BotonPausa.SetActive(true);
+        MenuPaus.SetActive(false);
+    }
+
+    public void Exit() 
+    {
+        Debug.Log("Cerrando Juego");
+        Application.Quit();
+    }
+
+
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey("q"))
+        {
+            BotonPausa.SetActive(true);
+            Time.timeScale = 0;
+
+        }
+    }
+}
